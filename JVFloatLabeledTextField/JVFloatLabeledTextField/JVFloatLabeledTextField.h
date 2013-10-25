6e5542ab7ceadf40138cd98e0445123b21687494
Christian Di Lorenzo <rcdilorenzo@me.com>
2013-10-25 14:39:44 -0400

    Allow setting y-padding between floating label and text field

--- JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.h
+++ JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.h
@@ -31,6 +31,7 @@
 @interface JVFloatLabeledTextField : UITextField
 
 @property (nonatomic, strong, readonly) UILabel * floatingLabel;
+@property (nonatomic, strong) NSNumber * floatingLabelYPadding UI_APPEARANCE_SELECTOR;
 @property (nonatomic, strong) UIFont * floatingLabelFont UI_APPEARANCE_SELECTOR;
 @property (nonatomic, strong) UIColor * floatingLabelTextColor UI_APPEARANCE_SELECTOR;
 @property (nonatomic, strong) UIColor * floatingLabelActiveTextColor UI_APPEARANCE_SELECTOR; // tint color is used by default if not provided
