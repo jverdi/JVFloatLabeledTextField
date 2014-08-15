32832a399dc0b01bae9704e7785f620b1e821bea
Jared Verdi <jared@jaredverdi.com>
2014-08-15 01:40:19 -0400

    Update licenses

--- JVFloatLabeledTextField/JVFloatLabeledTextField/NSString+TextDirectionality.m
+++ JVFloatLabeledTextField/JVFloatLabeledTextField/NSString+TextDirectionality.m
@@ -2,9 +2,30 @@
 //  NSString+TextDirectionality.m
 //  JVFloatLabeledTextField
 //
-//  Created by Benjamin Briggs on 19/06/2014.
-//  Based on a Stack Overflow answer http://stackoverflow.com/a/13697277/900251
+//  The MIT License (MIT)
 //
+//  Copyright (c) 2013 Jared Verdi
+//  Original Concept by Matt D. Smith
+//  http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+//  Based on a Stack Overflow answer http://stackoverflow.com/a/13697277/900251
 
 #import "NSString+TextDirectionality.h"
 
