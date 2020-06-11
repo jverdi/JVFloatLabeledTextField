aba62b7abada9e65c7b3fff79e13f16d719aba44
Oleg Langer <piligrimo@gmail.com>
2020-06-11 12:42:30 +0200

    Clean Up

--- Package.swift
+++ Package.swift
@@ -9,18 +9,14 @@ let package = Package(
         .iOS(.v8)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "JVFloatLabeledTextField",
             targets: ["JVFloatLabeledTextField"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JVFloatLabeledTextField",
             dependencies: [],
