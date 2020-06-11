9b23a642b08febe7aef73b87eb3be8b71cbfe023
Oleg Langer <piligrimo@gmail.com>
2020-06-11 08:57:36 +0200

    swift package init

---
+++ Sources/JVFloatLabeledTextField/JVFloatLabeledTextField.swift
@@ -0,0 +1,3 @@
+struct JVFloatLabeledTextField {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import JVFloatLabeledTextFieldTests
+
+var tests = [XCTestCaseEntry]()
+tests += JVFloatLabeledTextFieldTests.allTests()
+XCTMain(tests)
