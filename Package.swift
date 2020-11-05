d23048c1d9961f9f3526c6c2b5aa91dd0a6640db
Daniel Ayala Jabon <dayalaja@everis.com>
2020-11-05 15:40:02 +0100

    updated packake to .iOS(.v9)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "JVFloatLabeledTextField",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         .library(
